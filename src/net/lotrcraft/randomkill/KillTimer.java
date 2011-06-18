35b83656ee1cccc62a4e5a19c71914eb56520d3b
Francesco Macagno <fjmacagno1@yahoo.com>
2011-06-18 09:11:38 -0700

    Fixed Randomness

--- src/net/lotrcraft/randomkill/KillTimer.java
+++ src/net/lotrcraft/randomkill/KillTimer.java
@@ -8,14 +8,14 @@ import org.bukkit.plugin.Plugin;
 
 public class KillTimer implements Runnable{
     private Plugin p;
+	Random r = new Random();
     public KillTimer(Plugin p) {
         this.p = p;
     }
     public void run() {
-    	
+		int randomnumber = r.nextInt((RKMain.max - RKMain.min) * 20) + RKMain.min * 20;
         Player[] players = Bukkit.getServer().getOnlinePlayers();
         players[new Random().nextInt(players.length)].damage(Integer.MAX_VALUE);
-        int randomnumber = RKMain.randomnumber;
 		Bukkit.getServer().getScheduler().registerSyncDelayedTask(p, new WarnTimer(p), randomnumber );
     }
 }
--- src/net/lotrcraft/randomkill/RKMain.java
+++ src/net/lotrcraft/randomkill/RKMain.java
@@ -13,12 +13,11 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RKMain extends JavaPlugin{
-	public static int randomnumber;
+	public static int randomnumber, max, min;
 	File file =new File("//RandomKill//config.yml");
 	Logger log = Logger.getLogger("minecraft");
 	//ConfigurationNode timeamt = this.getConfiguration();
 	Configuration config = this.getConfiguration();
-	Random r = new Random();
 	
 	@Override
 	public void onDisable() {
@@ -47,10 +46,9 @@ public class RKMain extends JavaPlugin{
 		}
 		config.save();
 		config.setHeader("Version 0.1");
-		int max = config.getInt("maximum", 120);
-		int min = config.getInt("minimum", 60);
-		randomnumber = r.nextInt((max - min) * 20) + min * 20;
-		int a;
+		max = config.getInt("maximum", 120);
+		min = config.getInt("minimum", 60);
+
 	}
 	
 }
