1c8bfded9b7199a7f6067d0670e491ee0fb94c9c
Francesco Macagno <fjmacagno1@yahoo.com>
2011-06-27 10:54:55 -0700

    Added major changes; Addded permissions structure, command structure, and list structure.

--- src/net/lotrcraft/randomkill/KillTimer.java
+++ src/net/lotrcraft/randomkill/KillTimer.java
@@ -11,19 +11,29 @@ public class KillTimer implements Runnable{
     private Plugin p;
     Logger log = Logger.getLogger("minecraft");
 	Random r = new Random();
+	//Player[] players = RKMain.players;
     public KillTimer(Plugin p) {
         this.p = p;
     }
     public void run() {
 		int randomnumber = r.nextInt((RKMain.max - RKMain.min) * 20) + RKMain.min * 20;
-        Player[] players = Bukkit.getServer().getOnlinePlayers();
-        if (players.length != 0){
-        	players[new Random().nextInt(players.length)].damage(100);
+        Player[] onlineplayers = Bukkit.getServer().getOnlinePlayers();
+        for (int i = 0; i <= onlineplayers.length; i++){
+        	
+        }
+        
+        if (onlineplayers.length != 0){
+        	Player sac = onlineplayers[new Random().nextInt(onlineplayers.length)];
         	Bukkit.getServer().broadcastMessage("A Sacrifice has been chosen!");
         	log.info("[RandomKill] A player has been exterminated.");
         } else log.info("[RandomKill] No players to kill.");
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(p, new WarnTimer(p), randomnumber - 600);
-        
+        /*
+         *   if (!(MyPlugin).permissionHandler.has(player, "a.custom.node")) {
+      return;
+  }
+
+         */
 		
     }
 }
--- src/net/lotrcraft/randomkill/RKMain.java
+++ src/net/lotrcraft/randomkill/RKMain.java
@@ -1,12 +1,27 @@
 package net.lotrcraft.randomkill;
 
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.util.config.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
 public class RKMain extends JavaPlugin{
+	public static PermissionHandler permissionHandler;
 	public static int randomnumber, max, min;
+	public static boolean useList;
+	public static List<String> players;
+	//public static Player[] players;
 	//File file =new File("//RandomKill//config.yml");
 	Logger log = Logger.getLogger("minecraft");
 	Random r = new Random();
@@ -37,13 +52,88 @@ public class RKMain extends JavaPlugin{
 			config.setProperty("maximum", config.getInt("minimum", 60));
 			config.setProperty("minimum", max);
 		}
-		//config.setHeader("Version 0.1");
+		if (config.getProperty("useList") == null){
+			config.setProperty("useList", false);
+		}
+		if (config.getProperty("Players") == null){
+			config.setProperty("Players", null);
+		}
+		players = config.getStringList("Players", null);
+		
+		config.setHeader("#Version 0.1");
 		config.save();
+		useList = config.getBoolean("useList", false); 
 		max = config.getInt("maximum", 120);
 		min = config.getInt("minimum", 60);
-		int randomnumber = r.nextInt((max - min) * 20) + min * 20;
-		this.getServer().getScheduler().scheduleSyncDelayedTask(this, new KillTimer(this), randomnumber);
+		randomnumber = r.nextInt((max - min) * 20) + min * 20;
+		this.getServer().getScheduler().scheduleSyncDelayedTask(this, new WarnTimer(this), randomnumber-30);
 		//Hello!
+		setupPermissions();
+
 	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		log.info(args[0]);
+		Player a = (Player) sender;
+		if (args.length > 2 || args.length < 0){
+			return false;
+		}
+		if (args[0].equals("add")){
+			if (!RKMain.permissionHandler.has(a, "RK.list.add") && !(sender instanceof ConsoleCommandSender)){
+				sender.sendMessage("[RandomKill] You dont have permission to use this command!");
+				return true;
+			}
+			if (players.contains(args[1])){
+				sender.sendMessage("[RandomKill] This player already added.");
+				return true;
+			}
+			players.add(args[1]);
+			config.setProperty("Players", players);
+			config.save();
+			sender.sendMessage("[RandomKill] Player " + args[1] + " added.");
+			return true;
+		}
+		if (args[0].equals("remove")){
+			if (!RKMain.permissionHandler.has(a, "RK.list.remove") && !(sender instanceof ConsoleCommandSender)){
+				sender.sendMessage("[RandomKill] You dont have permission to use this command!");
+				return true;
+			}
+			if (!players.contains(args[1])){
+				sender.sendMessage("[RandomKill] Player not found.");
+				return true;
+			}
+			players.remove(args[1]);
+			config.setProperty("Players", players);
+			config.save();
+			sender.sendMessage("[RandomKill] Player " + args[1] + " removed.");
+			return true;
+		}
+		if(args[0].equals("kill")){
+			if (!RKMain.permissionHandler.has(a, "RK.kill") && !(sender instanceof ConsoleCommandSender)){
+				sender.sendMessage("[RandomKill] You dont have permission to use this command!");
+				return true;
+			}
+			Player p = Bukkit.getServer().matchPlayer(args[1]).get(0);
+			p.damage(100);
+		}
+		return false;
+	}
+	
+	private void setupPermissions() {
+	    if (permissionHandler != null) {
+	        return;
+	    }
+	    
+	    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+	    
+	    if (permissionsPlugin == null) {
+	        log.info("Permission system not detected, defaulting to OP");
+	        return;
+	    }
+	    
+	    permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+	    log.info("Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+	}
+
 	
 }
